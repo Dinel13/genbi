875583adda86dcbaeaa13d0e6a9fd7928dff4e91
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-03-19 21:40:18 +0800

    change origin header

--- app.js
+++ app.js
@@ -19,14 +19,13 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://genbisulsel.cyou");
   res.header(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     return res.sendStatus(200);
   }
   next();
